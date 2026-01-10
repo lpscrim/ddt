c0fca3f38ffe44b3e954a392cbb71c960b6571ff
Lewis Scrimgeour <112709088+lpscrim@users.noreply.github.com>
2026-01-10 15:25:39 +0000

    Refactor code structure for improved readability and maintainability

--- app/components/Sections/Hero.tsx
+++ app/components/Sections/Hero.tsx
@@ -2,16 +2,27 @@ import Image from 'next/image';
 
 export function Hero() {
   return (
-    <section id="home" className="min-h-svh flex flex-col items-center justify-center sm:px-4">
+    <section id="home" className="min-h-svh flex flex-col justify-center ">
       {/* Top image strip */}
-      <div className="w-full relative h-120 overflow-hidden">
+      <div className="hidden md:block w-full relative h-[27lvw]  overflow-hidden">
         <Image 
           src="/tree1.JPG" 
           alt="Tree background" 
           fill 
           className="object-cover object-top"
         />
-        <h2 className="absolute inset-0 flex items-center justify-center text-7xl sm:text-[236px] font-mono tracking-widest font-bold text-white/60">
+        <h2 className="absolute inset-0 flex items-center justify-center text-6xl sm:text-7xl md:text-8xl lg:text-9xl xl:text-[160px] 2xl:text-[200px] font-mono tracking-widest font-bold text-background">
+          DAYDREAMTEAM
+        </h2>
+      </div>
+      <div className="block md:hidden w-full relative h-[27lvw]  overflow-hidden">
+        <Image 
+          src="/pic1.JPG" 
+          alt="Tree background" 
+          fill 
+          className="object-cover object-top"
+        />
+        <h2 className="absolute inset-0 flex items-center justify-center text-6xl sm:text-7xl md:text-8xl lg:text-9xl xl:text-[160px] 2xl:text-[200px] font-mono tracking-widest font-bold text-background">
           DAYDREAMTEAM
         </h2>
       </div>
